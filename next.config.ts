d1f4a8381eeb11b850dba5e6a2b6c567b603665a
Katie <34925136+katiejyoung@users.noreply.github.com>
2025-04-03 10:18:25 -0700

    Cleanup

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  // basePath: "/katiejyoung",
-  // output: "export",  // enables github hosting
-  // reactStrictMode: true,
 };
 
 export default nextConfig;
