5c290af53d438f9dcf30e67407cfd18a7cea2720
Katie <34925136+katiejyoung@users.noreply.github.com>
2025-04-03 09:46:29 -0700

    Wowzer

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  basePath: "/personal-site",
-  output: "export",  // enables github hosting
-  reactStrictMode: true,
+  // basePath: "/katiejyoung",
+  // output: "export",  // enables github hosting
+  // reactStrictMode: true,
 };
 
 export default nextConfig;
