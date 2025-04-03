1422bd2d9c1cf49bdf35aecde38288e5f839e37b
Katie <34925136+katiejyoung@users.noreply.github.com>
2025-04-02 19:01:25 -0700

    Attempting to publish

--- personal-site/next.config.ts
+++ personal-site/next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  basePath: "/personal-site",
+  output: "export",  // enables github hosting
+  reactStrictMode: true,
 };
 
 export default nextConfig;
